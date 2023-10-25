2b7bc7b62519eb4d772eb21984767337e974595c
Marcel Wiebenga <marcel@deboprojects.nl>
2023-10-25 14:41:42 +0200

    Added provider (for use with Laravel)

---
+++ src/Instagram/InstagramGraphAPIServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Flarone\Instagram;
+
+use Illuminate\Support\ServiceProvider;
+
+class InstagramGraphAPIServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
